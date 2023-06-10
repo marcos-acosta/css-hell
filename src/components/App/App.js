3e19f71946a776633c79a937296a4bcb58a6ea3d
Marcos Acosta <macosta1625@gmail.com>
2023-06-10 01:20:09 -0400

    Add dev option for not updating cookies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import GameLogic from "../GameLogic/GameLogic";
 import { useCookies } from "react-cookie";
 
+const _DONT_UPDATE_COOKIES = true;
+
 export default function App() {
   const [cookies, setCookie] = useCookies(["checkpoint"]);
 
   const saveLevelCheckpoint = (levelNumber) =>
-    setCookie("checkpoint", levelNumber);
+    !_DONT_UPDATE_COOKIES && setCookie("checkpoint", levelNumber);
 
   const initLevelNumber = parseInt(cookies.checkpoint);
 
