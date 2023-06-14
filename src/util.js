231a5289b87827208224978d4e8ed3f1ec83f477
Marcos Acosta <macosta1625@gmail.com>
2023-06-13 20:32:57 -0400

    Remove some util functions

--- src/util.js
+++ src/util.js
@@ -1,5 +1,3 @@
-import overlap from "polygon-overlap";
-
 const LEVEL_DATA_PATH = "data/levels.json";
 const ELEMENT_TYPE = {
   peg: "p",
@@ -9,18 +7,6 @@ const ELEMENT_TYPE = {
 
 function combineClassNames(...classes) {
   return classes.filter(Boolean).join(" ");
-}
-
-function doBboxesOverlap(bbox1, bbox2) {
-  return bbox1 && bbox2 && overlap(extractPoints(bbox1), extractPoints(bbox2));
-}
-
-function extractPoints(bbox) {
-  let points = [];
-  Object.values(bbox).forEach((coords) => {
-    points.push([coords.left, coords.top]);
-  });
-  return points;
 }
 
 function matchingIdToLetter(matchingId) {
@@ -66,28 +52,6 @@ function testForOverlapRandom(holeElement, numPoints) {
   return false;
 }
 
-function testForOverlap(holeElement, pegId, granularity) {
-  if (!holeElement) {
-    return false;
-  }
-  const _granularity = granularity || 15;
-  const { left, right, top, bottom, height, width } =
-    holeElement.getBoundingClientRect();
-  let overlappingElements;
-  for (let x = left; x <= right; x += width / _granularity) {
-    for (let y = top; y <= bottom; y += height / _granularity) {
-      overlappingElements = document.elementsFromPoint(x, y);
-      if (
-        overlappingElements.some((element) => element.id === pegId) &&
-        overlappingElements.some((element) => element.id === holeElement.id)
-      ) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
 function isHole(id) {
   return id && id[0] === ELEMENT_TYPE.hole;
 }
@@ -95,9 +59,7 @@ function isHole(id) {
 export {
   LEVEL_DATA_PATH,
   combineClassNames,
-  doBboxesOverlap,
   interpretId,
-  testForOverlap,
   testForOverlapRandom,
   isHole,
   getIndexFromId,
